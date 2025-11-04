be135a772155e6ef38347444587f55a7936138a3
Leon Motaung <80112085+LeonMotaung@users.noreply.github.com>
2025-11-04 21:08:07 +0200

    Update and rename processing.py to binarise.py

---
+++ chapter2/binarise.py
@@ -0,0 +1,9 @@
+
+import numpy as np 
+from sklearn import preprocessing
+
+#this is the array of data and set the threshold
+input_data=np.array([[5.1,-2.9,3.3],[-1.2,7.8,-6.1],[3.9,0.4,2.1],[7.3,-9.9,-4.5]])
+
+data_binarized=preprocessing.Binarizer(threshold=2.1).transform(input_data)
+print("\n Binarized data: \n",data_binarized) 
